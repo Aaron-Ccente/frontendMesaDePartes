4227492c7d04f054de93cd88dd280b440ce513b2
Aaron-Ccente <aronccente@gmail.com>
2025-10-18 23:56:59 -0500

    Componentes nuevos

---
+++ src/components/admin/SystemConfiguration/GradosManagement.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function GradosManagement() {
+  return (
+    <div>GradosManagement</div>
+  )
+}
+
+export default GradosManagement
---
+++ src/components/admin/SystemConfiguration/TurnosManagement.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TurnosManagement() {
+  return (
+    <div>TurnosManagement</div>
+  )
+}
+
+export default TurnosManagement
