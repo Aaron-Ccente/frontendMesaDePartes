cba70cdaffbf79a51b45254b2163a13fc977b373
Aaron-Ccente <aronccente@gmail.com>
2025-10-22 00:04:11 -0500

    react eliminado - servicio para la gestion de los turnos actualizado

--- src/components/admin/SystemConfiguration/TurnosManagement.jsx
+++ src/components/admin/SystemConfiguration/TurnosManagement.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import ShowToast from '../../ui/ShowToast'
 import TurnoForm from './Forms/TurnoForm'
-import { turnoService } from '../../../services/turnoService'
+import turnoService  from '../../../services/turnoService'
 
 function TurnosManagement() {
   const [turnos, setTurnos] = useState([])
